4d6f7485becb6f354cd25ced71bc8d2b7ef58d83
vvalcarcell <valcarcellvalentina@gmail.com>
2021-06-15 19:27:54 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var coupon = 'SEICARO';
 var discount = 0.1;
 
 var result = 50;
-document.getElementById('final-price').innerHTML = 'Your Price is € ' + result;
+document.getElementById('final-price').innerHTML = '€ ' + result;
 
 calculate.addEventListener('click', function () {
 
@@ -20,11 +20,10 @@ calculate.addEventListener('click', function () {
 
     }
 
-    document.getElementById('final-price').innerHTML = 'Your Price is € ' + result;
+    document.getElementById('final-price').innerHTML = '€ ' + result;
 
     if (dCoupon.value.length > 0 && dCoupon.value === coupon) {
-        var dPrice = (result - (result * discount)).toFixed(2);
-        document.getElementById('d-price').innerHTML = 'But only for you the special price is € ' + dPrice + '!';
+        document.getElementById('final-price').innerHTML = '€ ' + (result - (result * discount)).toFixed(2);
     } else if (dCoupon.value.length > 0 && dCoupon.value !== coupon) {
         alert('Attenzione! Il tuo Coupon non è valido!')
     }
